7ec18e214965e6900d66f63d58e41c7a9df6e464
ctron <ctron@users.noreply.github.com>
2024-06-27 11:56:56 +0000

    deploy: 9c8dcc592deed8837945b6bd22980ea1e8085428

--- assets/js/3a2db09e.2cc961b1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktrustification_io=self.webpackChunktrustification_io||[]).push([[8121],{8070:s=>{s.exports=JSON.parse('{"tags":[{"label":"cyclonedx","permalink":"/blog/tags/cyclonedx","count":3},{"label":"sigstore","permalink":"/blog/tags/sigstore","count":4}]}')}}]);
